2d6f9abf87e62b7342b836a6febe308f4feb14f9
flyingnosky <flyingnosky@163.com>
2015-04-02 20:33:10 +0800

    add main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: main.c
+	> Author: 
+	> Mail: 
+	> Created Time: Thursday, April 02, 2015 PM08:31:47 HKT
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+    printf("github remote push!");
+    return 0;
+}
