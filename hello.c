25c35008beb931fa0af9b04aeb41cc2c889207cf
flyingnosky <flyingnosky@163.com>
2015-04-15 20:10:24 +0800

    add hello.c

---
+++ hello.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: 
+	> Mail: 
+	> Created Time: Wednesday, April 15, 2015 PM08:08:21 HKT
+ ************************************************************************/
+
+#include<stdio.h>
+hello()
+{
+    printf("This is a tag example!");
+}
